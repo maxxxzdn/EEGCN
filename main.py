3fa435cbf7467f4e336408e8d13df5c7b44c0bc1
Maksim Zhdanov <50237680+maxxxzdn@users.noreply.github.com>
2021-05-11 12:20:39 +0200

    Last generation

--- main.py
+++ main.py
@@ -1,129 +1,197 @@
 import torch
 import glob
 import argparse
-import numpy as np
 import random
+import numpy as np
+from torch_geometric.data import Data, DataLoader
+from torch_geometric.utils import to_networkx
 import torch.nn.functional as F
 from model import *
-from utils import *
-from TrajectoryDataset import TrajectoryDataset
-from torch_geometric.data import DataLoader
+from utils import train, test
 
-parser = argparse.ArgumentParser(description='EEG Signal Backward Euler ')
-parser.add_argument(
-    '--path_data',
-    type=str,
-    default='../EEG_data/data/*/*/*/*')
-parser.add_argument('--exp_name', type=str, default='euler',
+parser = argparse.ArgumentParser(description='EEG Signal Classification')
+parser.add_argument('--exp_name', type=str, default='exp',
                     help='Name of the experiment')
-parser.add_argument('--epochs', type=int, default=300,
-                    help='Number of epochs to train. 0 leads to training until reaching train accuracy = 1.')
-parser.add_argument('--experiments', type=int, default=2000,
-                    help='Number of experiments to train on')
-parser.add_argument('--batch_size', type=int, default=32,
+parser.add_argument('--train_dataset', type=str, default='datasets/train_data_6.pt',
+                    help='Data to train on')
+parser.add_argument('--val_dataset', type=str, default='datasets/val_data_6.pt',
+                    help='Data to validate on')
+parser.add_argument('--edge_index', type=str, default='edge_index3.txt',
+                    help='Conenctivity data')
+parser.add_argument('--model', type=str, default='gcn',
+                    choices=['gcn', 'cnn', 'mlp'],
+                    help='Model to use, [GCN, CNN+MLP, MLP]')
+parser.add_argument('--batch_size', type=int, default=32, 
                     help='Size of batch')
-parser.add_argument('--classifier', type=str, default='classifier.pt',
-                    help='path to saved parameters of the pretrained classifier')
+parser.add_argument('--epochs', type=int, default=100,
+                    help='Number of epochs to train. 0 leads to training until reaching train accuracy = 1.')
+parser.add_argument('--optimizer', type=str, default='adam', 
+                    choices=['adam', 'wadam', 'sgd'],
+                    help='Optimizer to use, [Adam, SGD]')
 parser.add_argument('--lr', type=float, default=1e-4,
                     help='learning rate (default: 5e-5, 5e-3 if using sgd)')
-parser.add_argument('--weight_decay', type=float, default=1e-4,
-                    help='Weight decay value for an optimizer')
+parser.add_argument('--momentum', type=float, default=0.9, 
+                    help='SGD momentum (default: 0.9)')
 parser.add_argument('--cuda', type=int, default=1,
                     choices=[0, 1],
                     help='Enables CUDA training')
 parser.add_argument('--horovod', type=int, default=0,
                     choices=[0, 1],
                     help='Enables Horovod parallelism')
-parser.add_argument('--wandb', type=int, default=0,
+parser.add_argument('--wandb', type=int, default=1,
                     choices=[0, 1],
                     help='Enables Weights and Biases tracking')
+
+parser.add_argument('--graph_pool', type=int, default=0,
+                    help='Pool the graph or not')
+parser.add_argument('--hidden_channels', type=int, default=128,
+                    help='Number of hidden channels of graph convolution layers')
 parser.add_argument('--num_features', type=int, default=64,
-                    help='Number of fetures assigned to each node')
-parser.add_argument('--hidden_size', type=int, default=128,
-                    help='Dimension the graph latent space')
-parser.add_argument('--hops', type=int, default=4,
-                    help='Number of message passing steps, >=1')
-parser.add_argument('--graph_info', type=str, default="../EEG_data/edge_index3.txt",
+                    help='Number of features to extract from a EEG signal')
+parser.add_argument('--pooling', type=str, default='avg',
+                    choices=['max', 'avg'],
+                    help='Pooling strategy to use, [Max, Average]')
+parser.add_argument('--activation', type=str, default='relu',
+                    choices=['leaky_relu', 'relu', 'tanh'],
+                    help='Activation function to use, [Leaky ReLU, ReLU, Tanh]')
+parser.add_argument('--hops', type=int, default=1,
+                    help='Hop distance in graph to collect information from, >=1')
+parser.add_argument('--layers', type=int, default=4,
+                    help='Classification layers in the model, >=1')                  
+parser.add_argument('--convs', type=int, default=6,
+                    help='Number of 1D convolutions to extract features from a signal, >=2')                
+parser.add_argument('--graph_info', type=str, default="Easycap_Koordinaten_61CH.txt",
                     help='File with infomation about node labels and their positions')
+parser.add_argument('--aggr', type=str, default='add',
+                    choices=['max', 'add'])
+parser.add_argument('--corr', type=int, default=0,
+                    choices=[0,1])
 args = parser.parse_args()
 
 # Use GPU if available and requested
 if args.cuda and torch.cuda.is_available():
     device = torch.device("cuda")
-    if args.horovod:  # Initialize horovod if requested
+    if args.horovod: # Initialize horovod if requested
         import horovod.torch as hvd
         hvd.init()
         torch.cuda.set_device(hvd.local_rank())
 else:
     device = torch.device("cpu")
 
-edge_index = np.loadtxt(args.graph_info)
+# Load dataset
+train_dataset = torch.load(args.train_dataset)
+random.shuffle(train_dataset)
+    
+val_dataset = torch.load(args.val_dataset)
+random.shuffle(val_dataset)
+
+#train_dataset = [data for data in train_dataset if data.y.item() in [0,1]]
+#val_dataset = [data for data in val_dataset if data.y.item() in [0,1]]
+# Investigate number of classes in dataset and length of signal
+classes = []
+for data in train_dataset[:100]:
+    if data.y.item() not in classes:
+        classes.append(data.y.item())
+n_classes = len(classes)
+signal_length = data.x.shape[1]
+
+# Add graph information to data
+positions = np.genfromtxt(args.graph_info)[:,0:3]
+positions = torch.tensor(positions)
+
+edge_index = np.loadtxt(args.edge_index)
 edge_index = torch.tensor(edge_index).long()
 
-model = Pipeline(args.hops, args.num_features, args.hidden_size)
+for data in train_dataset:
+    data.edge_index = edge_index
+    data.positions = positions
+for data in val_dataset:
+    data.edge_index = edge_index
+    data.positions = positions
+
+# Create batches with DataLoader
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
+
+# Initialize the model 
+if args.model == 'gcn':
+    model = GCN(hidden_channels=args.hidden_channels, num_features = args.num_features, n_classes = n_classes, hops = args.hops, layers = args.layers, convs = args.convs, pooling = args.pooling, aggr = args.aggr, corr = args.corr, device = device)
+elif args.model == 'mlp':
+    model = MLP()
+elif args.model == 'cnn':
+    model = CNN_MLP()
+
+# Start wandb tracking if requested
+if args.wandb:
+    import wandb
+    if not args.horovod or (args.horovod and hvd.rank()) == 0:
+        wandb.init(project="eegcn")
+        wandb.run.name = args.exp_name
+        wandb.config.update(args, allow_val_change=True)
+        wandb.config.update({"n_classes": n_classes})
+        wandb.watch(model)
+   
+# Send model to GPU or CPU
 model = model.double().to(device)
 
-model.load_state_dict(torch.load('classifier.pt'))
-
-# Load dataset
-experiments = list(glob.glob(args.path_data))[:args.experiments]
-
-train_dataset = TrajectoryDataset(experiments, edge_index)
-
-# Split dataset into two: one for training the model and one for testing it
-val_dataset = train_dataset[int(0.85 * (len(train_dataset))):]
-train_dataset = train_dataset[:int(0.85 * (len(train_dataset)))]
+my_list = ['node_bias']
+params = list(map(lambda x: x[1],list(filter(lambda kv: kv[0] in my_list, model.named_parameters()))))
+base_params = list(map(lambda x: x[1],list(filter(lambda kv: kv[0] not in my_list, model.named_parameters()))))
 
 # Initialize optimizer
-optimizer = torch.optim.Adam(
-    list(model.processor.parameters()) + list(model.decoder.parameters()),
-    lr=args.lr,
-    weight_decay=args.weight_decay)
+if args.optimizer == 'adam':
+    optimizer = torch.optim.Adam([{'params': base_params, 'lr': args.lr}, {'params': params, 'lr': args.lr}])
+elif args.optimizer == 'sgd':
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
+# Distribute parameters across resources
 if args.horovod:
-    optimizer = hvd.DistributedOptimizer(
-        optimizer,
-        named_parameters=model.named_parameters(),
-        backward_passes_per_step=1)
+    optimizer = hvd.DistributedOptimizer(optimizer, named_parameters=model.named_parameters(), backward_passes_per_step=1)
     hvd.broadcast_optimizer_state(optimizer, root_rank=0)
     hvd.broadcast_parameters(model.state_dict(), root_rank=0)
 
-# Train the model
+criterion = torch.nn.CrossEntropyLoss()
+
+# Train the model 
+train_acc = 0
+best_acc = 0
 epoch = 1
 
-while train_acc < 0.99:
-    loss = 0
-    MSE_train = 0
-    for i in range(0, len(train_dataset), args.batch_size):
-        loss += train(model,
-                      optimizer,
-                      train_dataset[i:(i + args.batch_size)],
-                      device)
-        with torch.no_grad():
-            MSE_train += test(model,
-                              train_dataset[i:(i + args.batch_size)],
-                              device)
-    with torch.no_grad():
-        MSE_val = test(model, val_dataset, device)
-    if args.wandb:  # Write down train/val accuracies, MSE values and loss
+while train_acc < 0.9999:
+    loss = train(model, optimizer, criterion, train_loader, epoch, device)
+    train_acc = test(model, train_loader, device)
+    test_acc = test(model, val_loader, device) 
+    if epoch%5==0:
+        for label in range(n_classes):
+            my_dataset = [data for data in val_dataset if data.y.item() == (label)]
+            my_loader = DataLoader(my_dataset, batch_size=args.batch_size, shuffle=True)
+            label_acc = test(model, my_loader, device)
+
+            if args.wandb: # Write down train/test accuracies and loss
+                if not args.horovod or (args.horovod and hvd.rank()) == 0:
+                    wandb.log({str(label) + " Accuracy": label_acc, "Epoch": epoch})
+
+   
+    if args.wandb: # Write down train/test accuracies and loss
         if not args.horovod or (args.horovod and hvd.rank()) == 0:
-            wandb.log({"Train Accuracy": train_acc,
-                       "Validation Accuracy": val_acc,
-                       "Validation Loss": loss,
-                       "Train MSE": MSE_train,
-                       "Validation MSE": MSE_val,
-                       "Epoch": epoch})
-    else:
-        if epoch % 1 == 0:
-            print(
-                f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train Acc: {train_acc:.4f}, Validation Acc: {val_acc:.4f}, MSE Train: {MSE_train:.4f}, MSE Val: {MSE_val:.4f}')
+            wandb.log({"Train Accuracy": train_acc, "Test Accuracy": test_acc, "Test Loss": loss, "Epoch": epoch})
+    else: 
+        if epoch%5==0:
+            print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')           
     if not args.horovod or hvd.rank() == 0:
-        if val_acc > best_acc:  # Save the best model and its accuracy result
-            best_acc = val_acc
+        if test_acc > best_acc: # Save the best model and its accuracy result
+            best_acc = test_acc
             if args.wandb:
                 wandb.log({"Best model accuracy": best_acc})
-    # If countable epoch number was given:
-    if args.epochs > 0 and epoch > args.epochs:
-        break
-
+            torch.save(model.state_dict(), str(args.model) + '_' + str(args.exp_name) + '.pt')
+    if args.epochs > 0 and epoch > args.epochs: # If countable epoch number was given:
+        break 
+        
     epoch += 1
+
+# Print best achieved result
+if args.wandb:
+    if not args.horovod or (args.horovod and hvd.rank()) == 0:
+        wandb.log({"Best model accuracy": best_acc, "Epoch": epoch})
+else:
+    print("Best model accuracy: " + str(round(best_acc,2)))
--- utils.py
+++ utils.py
@@ -1,127 +1,52 @@
-import torch.nn.functional as F
-
-# Functions to train and estimate the Euler model
-
-
-def train(model, optimizer, dataset, device):
-    """Function to train the Euler model on a dataset.
-    It trains the model on each pair of consequent states from the dataset one single time.
-    For each pair it calculates the loss and performs the optimizer step.
-
-    Args:
-        model: An instance of Pipeline class.
-        optimizer: Pytorch torch.optim algorithm.
-        dataset: List of lists containing two torch_geometric.data.Data objects
-        device: torch.device object
-
-    Returns:
-        loss value calculated on the whole dataset.
-    """
-
-    model.train()
-    model.encoder.eval()  # Make the model encoder deterministic
-
+import torch
+import numpy as np
+"""
+Functions to train and estimate a model
+"""
+def train(model, optimizer, criterion, train_loader, epoch, device):
+    model.train() # set training mode for the model
+    
+    if epoch % 100 == 0 and optimizer.param_groups[0]['lr'] > 1e-6:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.5 * param_group['lr']
+            
     loss_all = 0
-    for pair in dataset:
-        current, next_ = pair
-        current = current.to(device)
-        next_ = next_.to(device)
-
-        update_x = model(
-            next_.x, next_.edge_index, next_.u, next_.batch)
-        x_predicted = update_x + model.encoder(next_.x).detach()
-        x_exact = model.encoder(current.x).detach()
-
-        loss = F.mse_loss(x_predicted, x_exact)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-        loss_all += loss.item()
-    return loss_all
-
-
-def test(model, dataset, device):
-    """Function to test a model on a dataset.
-    It calculates mean square error between estimated current state and correct one.
-
-    Args:
-        model: An instance of Pipeline class.
-        dataset: List of lists containing two torch_geometric.data.Data objects
-        device: torch.device object
-
-    Returns:
-        Total MSE: sum of MSE values calculated for each pair of consequent states in the dataset.
-    """
-    model.eval()
-
-    mse = 0
-    for pair in dataset:
-        current, next_ = pair
-        current = current.to(device)
-        next_ = next_.to(device)
-
-        update_x = model(
-            next_.x, next_.edge_index, next_.u, next_.batch)
-        x_predicted = update_x + model.encoder(next_.x)
-        x_exact = model.encoder(current.x)
-        mse += F.mse_loss(x_predicted, x_exact)
-
-    return mse
-
-# Functions to train and estimate a classifier for the Euler model
-
-
-def classifier_train(model, optimizer, criterion, loader, device):
-    """Function to train a classification model on a loader.
-    It trains the model on mini-batches provided by the loader.
-    For each mini-batch it calculates the loss and performs the optimizer step.
-
-    Args:
-        model: classifier attribute of an instance of Pipeline class.
-        optimizer: Pytorch torch.optim algorithm.
-        criterion: torch.nn criterion
-        loader: torch_geometric.data.DataLoader object containing graphs
-        device: torch.device object
-
-    Returns:
-        loss value calculated on the whole loader.
-    """
-    model.train()
-
-    loss_all = 0
-    for data in loader:
+    for data in train_loader:  # Iterate in batches over the training dataset.
         data = data.to(device)
-
-        out = model(data.x, data.edge_index, data.batch)
-
-        loss = criterion(out, data.y)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-        loss_all += loss.item()
+        out = model(x = data.x, edge_index = data.edge_index, batch = data.batch)  # Perform a single forward pass.
+        loss = criterion(out, data.y)  # Compute the loss.
+        loss.backward()  # Derive gradients.
+        optimizer.step()  # Update parameters based on gradients.
+        optimizer.zero_grad()  # Clear gradients.
+        loss_all += loss.item()       
     return loss_all
 
-
-def classifier_test(model, loader, device):
-    """Function to test a classification model on a loader.
-    It calculates accuracy of the model prediction on the whole loader .
-
-    Args:
-        model: An instance of Pipeline class.
-        loader: torch_geometric.data.DataLoader object containing graphs
-        device: torch.device object
-
-    Returns:
-        percentage of correct predictions
-    """
-    model.eval()
+def test(model, loader, device):
+    model.eval() # set evaluation mode for the model
 
     correct = 0
-    for data in loader:
+    for data in loader:  # Iterate in batches over the training/test dataset.
         data = data.to(device)
+        out = model(x = data.x, edge_index = data.edge_index, batch = data.batch)
+        pred = out.argmax(dim=1)  # Use the class with highest probability.
+        correct += int((pred == data.y).sum())  # Check against ground-truth labels.
+    return correct / len(loader.dataset)  # Derive ratio of correct predictions.
 
-        out = model(data.x, data.edge_index, data.batch)
+def corr2_coeff(A, B):
+    # Rowwise mean of input arrays & subtract from input arrays themeselves
+    A_mA = A - A.mean(1)[:, None]
+    B_mB = B - B.mean(1)[:, None]
 
-        pred = out.argmax(dim=1)
-        correct += int((pred == data.y).sum())
-    return correct / len(loader.dataset)
+    # Sum of squares across rows
+    ssA = (A_mA**2).sum(1)
+    ssB = (B_mB**2).sum(1)
+
+    # Finally get corr coeff
+    return torch.diag((A_mA@B_mB.T) / torch.sqrt((ssA[:, None]@ssB[None])))
+
+def init_weights(m):
+    if type(m) == torch.nn.Linear:
+        torch.nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+    elif type(m) == torch.nn.BatchNorm1d:
+        torch.nn.init.normal_(m.weight, 1.0, 0.02)
+        torch.nn.init.zeros_(m.bias)
