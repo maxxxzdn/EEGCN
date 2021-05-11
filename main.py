b72911a387b71494e7b680a5675f808f4a3f9442
Maksim Zhdanov <50237680+maxxxzdn@users.noreply.github.com>
2021-05-11 12:21:04 +0200

    Last generation

--- main.py
+++ main.py
@@ -1,74 +1,71 @@
 import torch
 import glob
 import argparse
-import numpy as np
 import random
-from torch_geometric.data import DataLoader
-from torch_geometric.data import Data
+import numpy as np
+from torch_geometric.data import Data, DataLoader
 from torch_geometric.utils import to_networkx
-from torch.nn import Linear, Conv1d, MaxPool1d
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv,GraphConv, GATConv
-from torch_geometric.nn import global_mean_pool, global_sort_pool, global_max_pool
-from model import GCN
-from utils import train, test, distance, edgeCreator
+from model import *
+from utils import train, test
 
 parser = argparse.ArgumentParser(description='EEG Signal Classification')
 parser.add_argument('--exp_name', type=str, default='exp',
                     help='Name of the experiment')
-parser.add_argument('--train_dataset', type=str, default='EEG_data/train_data.pt',
+parser.add_argument('--train_dataset', type=str, default='datasets/train_data_6.pt',
                     help='Data to train on')
-parser.add_argument('--val_dataset', type=str, default='',
+parser.add_argument('--val_dataset', type=str, default='datasets/val_data_6.pt',
                     help='Data to validate on')
+parser.add_argument('--edge_index', type=str, default='edge_index3.txt',
+                    help='Conenctivity data')
 parser.add_argument('--model', type=str, default='gcn',
-                    choices=['gcn', 'mp'],
-                    help='Model to use, [GCN, MP]')
-parser.add_argument('--graph_operator', type=str, default='GraphConv',
-                    choices=['GCNConv', 'GraphConv', 'GATConv'],
-                    help='Model to use, [GCN, MP]')
+                    choices=['gcn', 'cnn', 'mlp'],
+                    help='Model to use, [GCN, CNN+MLP, MLP]')
 parser.add_argument('--batch_size', type=int, default=32, 
                     help='Size of batch')
 parser.add_argument('--epochs', type=int, default=100,
                     help='Number of epochs to train. 0 leads to training until reaching train accuracy = 1.')
 parser.add_argument('--optimizer', type=str, default='adam', 
-                    choices=['adam', 'sgd'],
+                    choices=['adam', 'wadam', 'sgd'],
                     help='Optimizer to use, [Adam, SGD]')
-parser.add_argument('--lr', type=float, default=0.00005,
+parser.add_argument('--lr', type=float, default=1e-4,
                     help='learning rate (default: 5e-5, 5e-3 if using sgd)')
 parser.add_argument('--momentum', type=float, default=0.9, 
                     help='SGD momentum (default: 0.9)')
-parser.add_argument('--weight_decay', type=float, default=1e-4,
-                    help='Weight decay value for an optimizer')
-parser.add_argument('--cuda', type=int, default=0,
+parser.add_argument('--cuda', type=int, default=1,
                     choices=[0, 1],
                     help='Enables CUDA training')
 parser.add_argument('--horovod', type=int, default=0,
                     choices=[0, 1],
                     help='Enables Horovod parallelism')
-parser.add_argument('--wandb', type=int, default=0,
+parser.add_argument('--wandb', type=int, default=1,
                     choices=[0, 1],
                     help='Enables Weights and Biases tracking')
-parser.add_argument('--hidden_channels', type=int, default=64,
+
+parser.add_argument('--graph_pool', type=int, default=0,
+                    help='Pool the graph or not')
+parser.add_argument('--hidden_channels', type=int, default=128,
                     help='Number of hidden channels of graph convolution layers')
 parser.add_argument('--num_features', type=int, default=64,
                     help='Number of features to extract from a EEG signal')
 parser.add_argument('--pooling', type=str, default='avg',
                     choices=['max', 'avg'],
                     help='Pooling strategy to use, [Max, Average]')
-parser.add_argument('--activation', type=str, default='leaky_relu',
+parser.add_argument('--activation', type=str, default='relu',
                     choices=['leaky_relu', 'relu', 'tanh'],
                     help='Activation function to use, [Leaky ReLU, ReLU, Tanh]')
-parser.add_argument('--hops', type=int, default=8,
+parser.add_argument('--hops', type=int, default=1,
                     help='Hop distance in graph to collect information from, >=1')
 parser.add_argument('--layers', type=int, default=4,
                     help='Classification layers in the model, >=1')                  
-parser.add_argument('--convs', type=int, default=5,
-                    help='Number of 1D convolutions to extract features from a signal, >=2')
-parser.add_argument('--explain', type=int, default=0,
-                    choices=[0,1],
-                    help='Calculate node importance using Integrated Gradients (captum)')                     
-parser.add_argument('--graph_info', type=str, default="",
+parser.add_argument('--convs', type=int, default=6,
+                    help='Number of 1D convolutions to extract features from a signal, >=2')                
+parser.add_argument('--graph_info', type=str, default="Easycap_Koordinaten_61CH.txt",
                     help='File with infomation about node labels and their positions')
+parser.add_argument('--aggr', type=str, default='add',
+                    choices=['max', 'add'])
+parser.add_argument('--corr', type=int, default=0,
+                    choices=[0,1])
 args = parser.parse_args()
 
 # Use GPU if available and requested
@@ -85,14 +82,11 @@ else:
 train_dataset = torch.load(args.train_dataset)
 random.shuffle(train_dataset)
     
-# Split dataset into two: one for training the model and one for testing it
-if len(args.val_dataset) == 0:
-    train_dataset = train_dataset[:int(0.85*(len(train_dataset)))] 
-    val_dataset = train_dataset[int(0.85*(len(train_dataset))):]
-else: 
-    val_dataset = torch.load(args.val_dataset)
-    random.shuffle(val_dataset)
+val_dataset = torch.load(args.val_dataset)
+random.shuffle(val_dataset)
 
+#train_dataset = [data for data in train_dataset if data.y.item() in [0,1]]
+#val_dataset = [data for data in val_dataset if data.y.item() in [0,1]]
 # Investigate number of classes in dataset and length of signal
 classes = []
 for data in train_dataset[:100]:
@@ -103,26 +97,29 @@ signal_length = data.x.shape[1]
 
 # Add graph information to data
 positions = np.genfromtxt(args.graph_info)[:,0:3]
-labels = np.genfromtxt(args.graph_info, 'str')[:,3]
 positions = torch.tensor(positions)
 
-edge_index = np.loadtxt('./EEG_data/edge_index3.txt')
+edge_index = np.loadtxt(args.edge_index)
 edge_index = torch.tensor(edge_index).long()
 
 for data in train_dataset:
     data.edge_index = edge_index
+    data.positions = positions
 for data in val_dataset:
     data.edge_index = edge_index
-    
+    data.positions = positions
+
 # Create batches with DataLoader
 train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
+val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
 
 # Initialize the model 
 if args.model == 'gcn':
-    model = GCN(hidden_channels=args.hidden_channels, num_features = args.num_features, n_classes = n_classes, graph_operator = args.graph_operator, hops = args.hops, layers = args.layers, convs = args.convs, activation = args.activation, pooling = args.pooling)
-elif args.model == 'mp':
-    raise NotImplementedError
+    model = GCN(hidden_channels=args.hidden_channels, num_features = args.num_features, n_classes = n_classes, hops = args.hops, layers = args.layers, convs = args.convs, pooling = args.pooling, aggr = args.aggr, corr = args.corr, device = device)
+elif args.model == 'mlp':
+    model = MLP()
+elif args.model == 'cnn':
+    model = CNN_MLP()
 
 # Start wandb tracking if requested
 if args.wandb:
@@ -137,11 +134,15 @@ if args.wandb:
 # Send model to GPU or CPU
 model = model.double().to(device)
 
+my_list = ['node_bias']
+params = list(map(lambda x: x[1],list(filter(lambda kv: kv[0] in my_list, model.named_parameters()))))
+base_params = list(map(lambda x: x[1],list(filter(lambda kv: kv[0] not in my_list, model.named_parameters()))))
+
 # Initialize optimizer
 if args.optimizer == 'adam':
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    optimizer = torch.optim.Adam([{'params': base_params, 'lr': args.lr}, {'params': params, 'lr': args.lr}])
 elif args.optimizer == 'sgd':
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
 # Distribute parameters across resources
 if args.horovod:
@@ -156,10 +157,21 @@ train_acc = 0
 best_acc = 0
 epoch = 1
 
-while train_acc < 0.99:
+while train_acc < 0.9999:
     loss = train(model, optimizer, criterion, train_loader, epoch, device)
     train_acc = test(model, train_loader, device)
-    test_acc = test(model, val_loader, device)    
+    test_acc = test(model, val_loader, device) 
+    if epoch%5==0:
+        for label in range(n_classes):
+            my_dataset = [data for data in val_dataset if data.y.item() == (label)]
+            my_loader = DataLoader(my_dataset, batch_size=args.batch_size, shuffle=True)
+            label_acc = test(model, my_loader, device)
+
+            if args.wandb: # Write down train/test accuracies and loss
+                if not args.horovod or (args.horovod and hvd.rank()) == 0:
+                    wandb.log({str(label) + " Accuracy": label_acc, "Epoch": epoch})
+
+   
     if args.wandb: # Write down train/test accuracies and loss
         if not args.horovod or (args.horovod and hvd.rank()) == 0:
             wandb.log({"Train Accuracy": train_acc, "Test Accuracy": test_acc, "Test Loss": loss, "Epoch": epoch})
@@ -180,12 +192,6 @@ while train_acc < 0.99:
 # Print best achieved result
 if args.wandb:
     if not args.horovod or (args.horovod and hvd.rank()) == 0:
-        wandb.log({"Best model accuracy": best_acc})
+        wandb.log({"Best model accuracy": best_acc, "Epoch": epoch})
 else:
     print("Best model accuracy: " + str(round(best_acc,2)))
-    
-# Explain graph network predictions using integrated gradients if requested
-if args.explain:
-    from utils import *
-    positions = positions[:,:2]
-    explain(model, dataset, positions, labels)
--- model.py
+++ model.py
@@ -1,84 +1,56 @@
 import torch
-from torch.nn import Linear, Conv1d, MaxPool1d, AvgPool1d, ModuleList
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv,GraphConv, GATConv
+from torch_geometric.nn import GraphConv, GATConv, ChebConv
 from torch_geometric.nn import global_mean_pool, global_max_pool
 from torch_geometric.utils import to_networkx
-from torch.nn import Linear, Conv1d, MaxPool1d, AvgPool1d, ModuleList, Sequential, ReLU, Dropout, BatchNorm1d
+from torch.nn import Linear, Conv1d, MaxPool1d, AvgPool1d, ModuleList, Dropout, BatchNorm1d, CosineSimilarity, Parameter
+from utils import corr2_coeff, init_weights
+from my_conv import MyzConv, GraphConvBias
 
-class GCN(torch.nn.Module):
-    def __init__(self, hidden_channels, num_features, n_classes, graph_operator, hops = 8, layers = 6, convs = 4, activation = 'relu', pooling = "avg"):
-        super(GCN, self).__init__()
+class MLP(torch.nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
         torch.manual_seed(12345)
-        
-        # check if architecture requested is meaningful
-        assert hops > 0
-        assert layers > 0
-        assert convs > 1
-        
-        self.hops = hops
-        self.layers = layers
-        self.convs = convs
-        self.n_classes = n_classes
-        self.num_features = num_features
-        self.activation = activation
 
-        # Graph operator choice
-        if graph_operator == 'GCNConv':
-            self.graph_operator = GCNConv
-        elif graph_operator == 'GraphConv':
-            self.graph_operator = GraphConv
-        elif graph_operator == 'GATConv':
-            self.graph_operator = GATConv
-        else: 
-            raise NotImplementedError  
+        self.layers= ModuleList([])
+        self.layers.append(Linear(61*200, 1000))
+        self.layers.append(Linear(1000, 256))
+        for _ in range(4):
+            self.layers.append(Linear(256, 256))
+        self.layers.append(Linear(256, 6))
 
-        # Activation function choice
-        if activation == 'leaky_relu':
-        	self.activation = F.leaky_relu
-        elif activation == 'relu':
-                self.activation = F.relu
-        elif activation == 'tanh':
-                self.activation = F.tanh
-        else: 
-            raise NotImplementedError
-            
-        # Pooling strategy choice
-        if pooling == "max":
-            self.pooling = MaxPool1d(kernel_size = 7)
-            self.graphPooling = global_max_pool
-        elif pooling == "avg":
-            self.pooling = AvgPool1d(kernel_size = 7)
-            self.graphPooling = global_mean_pool
-        else:
-            raise NotImplementedError
-                    
-        # 1D Convolutions to extract features from a signal                    
+    def forward(self, x, edge_index, batch):
+        x = x.reshape((batch.max().item()+1),-1)     
+        for layer in self.layers[:-1]:
+            x = layer(x)
+            x = F.relu(x)
+        x = self.layers[-1](x)    
+        return F.log_softmax(x, -1) 
+
+class CNN_MLP(torch.nn.Module):
+    def __init__(self):
+        super(CNN_MLP, self).__init__()
+        torch.manual_seed(12345)
+        self.activation = F.relu
+        self.pooling = AvgPool1d(kernel_size = 7)
         self.convs1d = ModuleList([])
-        self.convs1d.append(Conv1d(in_channels = 1, out_channels = self.num_features, kernel_size = 7))
-        for _ in range(self.convs-2):
-            self.convs1d.append(Conv1d(in_channels = self.num_features, out_channels = self.num_features, kernel_size = 5, padding = 2))
-        self.convs1d.append(Conv1d(in_channels = self.num_features, out_channels = self.num_features, kernel_size = 3))
-        
-        # Graph Convolution Networks
-        self.gconvs = ModuleList([])
-        self.gconvs.append(GraphConv(self.num_features, hidden_channels))
-        for _ in range(self.hops-1):
-            self.gconvs.append(GraphConv(hidden_channels, hidden_channels))
-	
-	# Linear layers to make a prediction  
-        self.linlayers = ModuleList([])
-        for _ in range(self.layers-1):
-            self.linlayers.append(Linear(hidden_channels, hidden_channels))
-        self.linlayers.append(Linear(hidden_channels, self.n_classes))
+        self.convs1d.append(Conv1d(in_channels = 1, out_channels = 128, kernel_size = 7))
+        for _ in range(2):
+            self.convs1d.append(Conv1d(in_channels = 128, out_channels = 128, kernel_size = 5, padding = 2))
+        self.convs1d.append(Conv1d(in_channels = 128, out_channels = 128, kernel_size = 3))
 
-    def forward(self, x, edge_index, batch, edge_weight = None):
+        self.layers = ModuleList([])
+        self.layers.append(Linear(61*128, 256))
+        for _ in range(2):
+            self.layers.append(Linear(256, 256))
+        self.layers.append(Linear(256, 6))
+
+    def forward(self, x, edge_index, batch):
 
         rows = x.shape[0] # number of signals in batch = 61 * graphs in batch
         cols = x.shape[1] # length of a signal
         x = x.reshape(rows,1,cols) # reshape to 1 channel
 
-        # 1. Extract features from time series
         x = self.convs1d[0](x)
         x = self.activation(x)
         x = self.pooling(x)
@@ -94,21 +66,116 @@ class GCN(torch.nn.Module):
         x = self.convs1d[-1](x)
         x = x.squeeze()
 
+        x = x.reshape((batch.max().item()+1),-1)     
+        for layer in self.layers[:-1]:
+            x = layer(x)
+            x = F.relu(x)
+        x = self.layers[-1](x)    
+        return F.log_softmax(x, -1) 
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels, num_features, n_classes, hops, layers, convs, pooling, aggr, corr, device):
+        super(GCN, self).__init__()
+        torch.manual_seed(12345)
+        
+        # check if architecture requested is meaningful
+        assert hops > 0
+        assert layers > 0
+        assert convs > 1
+        assert aggr in ['max', 'add']
+        
+        self.hops = hops
+        self.layers = layers
+        self.convs = convs
+        self.n_classes = n_classes
+        self.num_features = num_features
+        self.hidden_channels = hidden_channels
+        self.activation = F.relu
+        self.gconv = MyzConv #GraphConvBias
+        self.aggr = aggr
+        self.corr = corr
+        
+        #self.node_bias = Parameter(torch.tensor(61*[-10.], requires_grad=True, device = device).reshape(-1,1))
+      
+        if pooling == "max":
+            self.pooling = MaxPool1d(kernel_size = 7)
+            self.graphPooling = global_max_pool
+        elif pooling == "avg":
+            self.pooling = AvgPool1d(kernel_size = 7)
+            self.graphPooling = global_mean_pool
+        else:
+            raise NotImplementedError          
+                    
+        # 1D Convolutions to extract features from a signal                    
+        self.convs1d = ModuleList([])
+        self.convs1d.append(Conv1d(in_channels = 1, out_channels = self.num_features, kernel_size = 7))
+        for _ in range(self.convs-2):
+            self.convs1d.append(Conv1d(in_channels = self.num_features, out_channels = self.num_features, kernel_size = 5, padding = 2))
+        self.convs1d.append(Conv1d(in_channels = self.num_features, out_channels = self.num_features, kernel_size = 3)) 
+               
+        self.bn1 = BatchNorm1d(self.num_features)
+                    
+        # Graph Convolution Networks   
+        self.gconvs = ModuleList([])
+        self.gconvs.append(self.gconv(self.num_features, self.hidden_channels, aggr = self.aggr))
+        for _ in range(self.hops-1):
+            self.gconvs.append(self.gconv(self.hidden_channels, self.hidden_channels, aggr = self.aggr))
+            
+        self.bn2 = BatchNorm1d(self.hidden_channels)
+                
+        # Linear layers to make a prediction  
+        self.linlayers = ModuleList([])
+        self.linlayers.append(Linear(61*self.hidden_channels, self.hidden_channels))
+        for _ in range(self.layers-2):
+            self.linlayers.append(Linear(self.hidden_channels, self.hidden_channels))
+        self.linlayers.append(Linear(self.hidden_channels, self.n_classes))     
+        
+
+    def forward(self, x, edge_index, batch):
+        
+        edge_weight = None
+        if self.corr:      
+            x_j = x[edge_index[0]]
+            x_i = x[edge_index[1]]
+            edge_weight = 1 - corr2_coeff(x_j, x_i).abs()
+
+        rows = x.shape[0] # number of signals in batch = 61 * graphs in batch
+        cols = x.shape[1] # length of a signal
+        x = x.reshape(rows,1,cols) # reshape to 1 channel
+
+        # 1. Extract features from time series
+        x = self.convs1d[0](x)
+        x = self.activation(x)
+        x = self.pooling(x)
+        
+        for conv1d in self.convs1d[1:-1]:
+            x = conv1d(x)
+            x = self.activation(x)
+            
+        x = self.pooling(x)
+        x = self.convs1d[-1](x)
+        x = x.squeeze()
+
+        x = self.bn1(x)
+        x = F.dropout(x, p=0.33, training = self.training)
+
         # 2. Obtain node embeddings; x.shape = [61,node_features]
         for gconv in self.gconvs[:-1]:
-            x = gconv(x, edge_index, edge_weight) #[61, hidden_channels]
+            x = gconv(x = x, edge_index = edge_index, edge_weight = edge_weight) #[61, hidden_channels]
             x = self.activation(x)
-            x = F.dropout(x, p=0.25, training = self.training)
-        x = self.gconvs[-1](x, edge_index, edge_weight)
+        x = self.gconvs[-1](x = x, edge_index = edge_index, edge_weight = edge_weight)
    
+        x = self.bn2(x)
+        x = self.activation(x)
         # 3. Readout layer
-        x = self.graphPooling(x, batch)  # [batch_size, hidden_channels]
-
+        x = x.reshape((batch.max().item()+1),-1)
+        
         # 4. Apply a final classifier
-        x = F.dropout(x, p=0.5, training=self.training)
+        x = F.dropout(x, p=0.33, training=self.training)
         for linlayer in self.linlayers[:-1]:
             x = linlayer(x)
             x = self.activation(x)
         x = self.linlayers[-1](x)
         
-        return F.softmax(x, -1) 
+        return F.log_softmax(x, -1) 
